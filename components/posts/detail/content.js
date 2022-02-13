63c807a6d606abc14ed77b96373074aad5479782
rkiel <cygnus@pobox.com>
2022-02-12 19:00:18 -0500

    190: done

--- components/posts/detail/content.js
+++ components/posts/detail/content.js
@@ -2,15 +2,8 @@ import ReactMarkdown from "react-markdown";
 import Header from "./header";
 import classes from "./content.module.css";
 
-const post = {
-  title: "Getting Started with NextJS",
-  image: "getting-started-nextjs.png",
-  date: "2022-02-10",
-  slug: "getting-started-with-nextjs",
-  content: "# This is a first post",
-};
-
-function Content() {
+function Content(props) {
+  const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,32 @@
 import PostContent from "../../components/posts/detail/content";
+import { getData, getFiles, getSlug } from "../../lib/posts";
 
-function Page() {
-  return <PostContent />;
+function Page(props) {
+  return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const data = getData(slug);
+
+  return {
+    props: { post: data },
+  };
+}
+
+export function getStaticPaths() {
+  const fileNames = getFiles();
+
+  const slugs = fileNames.map((fileName) => getSlug(fileName));
+
+  const paths = slugs.map((slug) => ({ params: { slug } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export default Page;
